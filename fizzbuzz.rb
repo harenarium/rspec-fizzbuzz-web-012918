1f7dd16d5ef3598fffe8df08bfb47f864dc82ba0
Caroline Lee <carolinel2610@gmail.com>
2018-01-09 05:29:58 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,5 +1,9 @@
 def fizzbuzz(int)
   if int % 3 == 0 && int % 5 == 0
-    "FizzBuzz"
+    return "FizzBuzz"
+  elsif int % 3 == 0
+    return "Fizz"
+  elsif int % 5 == 0
+    return "FizzBuzz"
   end
 end
